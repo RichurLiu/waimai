b71b2d4e15f3a84b63a78ee5ce7dca23440525f7
刘磊 <liulei01@bilibili.com>
2021-02-13 20:04:43 +0800

    first commit

---
+++ gshop/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <!-- 显示路由组件 -->
+     <router-view></router-view>
+     <FooterGuide/>
+  </div>
+</template>
+
+<script>
+import FooterGuide from './components/FooterGuide/FooterGuide'
+export default {
+  name: 'App',
+  components:{
+    FooterGuide
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+#app
+  width 100%
+  height 100%
+  background-color #f5f5f5
+  position relative
+</style>
